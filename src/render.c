1084cbeed3aa809cf65427d34140a76af620a2f3
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-03-18 13:46:26 +0100

    add background

--- src/render.c
+++ src/render.c
@@ -6,11 +6,27 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 11:47:45 by anfouger          #+#    #+#             */
-/*   Updated: 2026/03/17 10:49:32 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/03/18 13:45:54 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
+
+static int	get_color(t_ray ray, t_data *data, t_sphere sphere)
+{
+	int	color;
+
+	if (hit_sphere(ray, sphere, data))
+		color = rgb_to_hex(255, 0, 0);
+	else
+	{
+		double t = 0.5 * (ray.dir.y + 1.0);
+		int blue = (int)(255 * t);
+		int white = (int)(255 * (1 - t));
+		color = rgb_to_hex(white, white, blue);
+	}
+	return (color);
+}
 
 void	render(t_data *data)
 {
@@ -27,8 +43,7 @@ void	render(t_data *data)
 		while (x < WIN_WIDTH)
 		{
 			ray = calc_ray(x_to_sx(x), y_to_sy(y), data);
-			put_pixel(data->mlx->img, x, y, 
-				rgb_to_hex(hit_sphere(ray, sphere, data) * 255, 0, 0));
+			put_pixel(data->mlx->img, x, y, get_color(ray, data, sphere));
 			x++;
 		}
 		y++;
