168953c8f93a0f7bb6f1be14d953ece945320377
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-05-10 11:02:53 +0200

    add function to print data to have faster testing phase

--- src/main.c
+++ src/main.c
@@ -6,23 +6,15 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 10:33:52 by anfouger          #+#    #+#             */
-/*   Updated: 2026/05/10 10:50:39 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/05/10 11:02:09 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
-
-void	print_data(t_data *data)
-{
-	printf("al.brightness = %f", data->scene.al.brightness);
-	printf("al.color: R = %f, G = %f, B = %f", data->scene.al.color.r);
-	printf("cam.pos = %f,%f,%f\n", data->scene.cam.pos.x, data->scene.cam.pos.y, data->scene.cam.pos.z);
-	printf("cam.n_orientation = %f,%f,%f\n", data->scene.cam.n_orientation.x, data->scene.cam.n_orientation.y, data->scene.cam.n_orientation.z);
-	printf("cam.fov = %i", data->scene.cam.fov);
-	printf("light.pos = %f,%f,%f\n", data->scene.light.pos.x, data->scene.light.pos.y, data->scene.light.pos.z);
-	printf("light.brightness = %f", data->scene.light.brightness);
-	printf("light.color: R = %f, G = %f, B = %f", data->scene.light.color);
-}
+void	print_al(t_al al);
+void	print_cam(t_cam cam);
+void	print_light(t_light light);
+void	print_data(t_data *data);
 
 int	main(int ac, char **av)
 {
@@ -42,4 +34,42 @@ int	main(int ac, char **av)
 	// set_controls(data);
 	// mlx_loop(data->mlx->mlx);
 	return (0);
+}
+
+void	print_al(t_al al)
+{
+	printf("// --- Ambient Light --- //\n");
+	printf("al.brightness = %f",
+		al.brightness);
+	printf("al.color: R = %f, G = %f, B = %f",
+		al.color.r, al.color.g, al.color.b);
+}
+
+void	print_cam(t_cam cam)
+{
+	printf("// --- Camera --- //\n");
+	printf("cam.pos = %f,%f,%f\n",
+		cam.pos.x, cam.pos.y, cam.pos.z);
+	printf("cam.n_orientation = %f,%f,%f\n",
+		cam.n_orientation.x, cam.n_orientation.y, cam.n_orientation.z);
+	printf("cam.fov = %i\n",
+		cam.fov);
+}
+
+void	print_light(t_light light)
+{
+	printf("// --- Light --- //\n");
+	printf("light.pos = %f,%f,%f\n",
+		light.pos.x,light.pos.y, light.pos.z);
+	printf("light.brightness = %f\n",
+		light.brightness);
+	printf("light.color: R = %f, G = %f, B = %f\n",
+		light.color.r, light.color.g, light.color.b);
+}
+
+void	print_data(t_data *data)
+{
+	print_al(data->scene.al);
+	print_cam(data->scene.cam);
+	print_light(data->scene.light);
 }
--- src/parsing/parsing_objects.c
+++ src/parsing/parsing_objects.c
@@ -6,45 +6,39 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 08:46:07 by anfouger          #+#    #+#             */
-/*   Updated: 2026/03/24 09:54:10 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/05/10 11:00:29 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-static int	parse_cylinder(t_data *data, char **tab)
+static void	parse_cylinder(t_data *data, char **tab)
 {
 	(void)data;
 	(void)tab;
 	printf("detect cylinder\n");
-	return (1);
 }
 
-static int	parse_sphere(t_data *data, char **tab)
+static void	parse_sphere(t_data *data, char **tab)
 {
 	(void)data;
 	(void)tab;
 	printf("detect sphere\n");
-	return (1);
 }
 
-static int	parse_plane(t_data *data, char **tab)
+static void	parse_plane(t_data *data, char **tab)
 {
 	(void)data;
 	(void)tab;
 	printf("detect plane\n");
-	return (1);
 }
 
-int	parse_objects(t_data *data, char **tab)
+void	parse_objects(t_data *data, char **tab)
 {
-	int	res;
-
 	if (!ft_strcmp(tab[0], "cy"))
-		res = parse_cylinder(data, tab);
+		parse_cylinder(data, tab);
 	else if (!ft_strcmp(tab[0], "sp"))
-		res = parse_sphere(data, tab);
+		parse_sphere(data, tab);
 	else
-		res = parse_plane(data, tab);
-	return (res);
+		parse_plane(data, tab);
 }
