72d7850136610d1f48f8361c450f5bd5acfb69ec
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-06-06 09:43:12 +0200

    add hit plane

--- src/graph_engine/color_utils.c
+++ src/graph_engine/color_utils.c
@@ -6,13 +6,13 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 10:53:55 by anfouger          #+#    #+#             */
-/*   Updated: 2026/05/19 09:10:19 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/06/03 11:03:48 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
-static t_color	color_inter_mult(t_color color1, t_color color2)
+static t_color	color_modulation(t_color color1, t_color color2)
 {
 	t_color	result;
 
@@ -30,7 +30,6 @@ t_color	color_mult(t_color color, double mult)
 	result.r = color.r * mult;
 	result.g = color.g * mult;
 	result.b = color.b * mult;
-	result.hex = rgb_to_hex((int)color.r, (int)color.g, (int)color.b);
 	color_clamp(&result);
 	return (result);
 }
@@ -42,7 +41,6 @@ static t_color	color_div(t_color color, double div)
 	result.r = color.r / div;
 	result.g = color.g / div;
 	result.b = color.b / div;
-	result.hex = rgb_to_hex((int)color.r, (int)color.g, (int)color.b);
 	color_clamp(&result);
 	return (result);
 }
@@ -61,7 +59,7 @@ t_color	color_mix(t_color color1, t_color color2, double brightness)
 
 	color1 = color_div(color1, 255);
 	color2 = color_div(color2, 255);
-	result = color_inter_mult(color1, color2);
+	result = color_modulation(color1, color2);
 	result = color_mult(result, brightness);
 	result = color_mult(result, 255);
 	color_clamp(&result);
--- src/graph_engine/plane.c
+++ src/graph_engine/plane.c
@@ -6,19 +6,41 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 08:48:00 by anfouger          #+#    #+#             */
-/*   Updated: 2026/05/19 09:06:47 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/06/06 09:36:53 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
+static double calc_dst(t_ray ray, t_pl *plane)
+{
+	double a;
+	double b;
+	double dst;
+
+	a = vec_dot(vec_sub(ray.origin, plane->pos), plane->n_vec);
+	b = vec_dot(ray.dir, plane->n_vec);
+	if (b == 0)
+		return (0);
+	dst = -a/b;
+	return (dst);
+}
+
 t_hit	hit_plane(t_ray ray, t_pl *plane)
 {
 	t_hit	hit;
+	double	dst;
 
-	(void)plane;
-	(void)ray;
-	hit.dst = -1;
-	hit.col_obj = init_color(0, 0, 0);
+	dst = calc_dst(ray, plane);
+	if (dst <= 0) 
+	{
+		hit.is_hit = 0;
+		return (hit);
+	}
+	hit.dst = dst;
+	hit.col_obj = plane->color;
+	hit.point = vec_add(ray.origin, vec_mult(ray.dir, hit.dst));
+	hit.normal = plane->n_vec;
+	hit.is_hit = 1;
 	return (hit);
 }
