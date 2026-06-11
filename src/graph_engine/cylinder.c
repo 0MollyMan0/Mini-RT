8646a9fd513f8fee709ef730a499709155fe35de
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-06-11 10:02:52 +0200

    first part of cylinder hit done

--- src/graph_engine/cylinder.c
+++ src/graph_engine/cylinder.c
@@ -6,19 +6,76 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 08:47:45 by anfouger          #+#    #+#             */
-/*   Updated: 2026/05/19 09:06:49 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/06/11 09:59:50 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
+double	calc_delta_cy(double a, double b, t_ray ray, t_cy *cy)
+{
+	double	c;
+	t_vec3	m;
+	double	r;
+
+	r = cy->dia * 0.5;
+	m = vec_sub(ray.origin, cy->pos);
+	c = vec_dot(m, m) - pow(vec_dot(m, cy->n_vec), 2) - pow(r, 2);
+	return (pow(b, 2) - 4 * a * c);
+}
+
+double	calc_b_cy(t_ray ray, t_cy *cy)
+{
+	t_vec3	m;
+	double	b;
+
+	m = vec_sub(ray.origin, cy->pos);
+	b = 2 * ((vec_dot(m, ray.dir)) -
+		vec_dot(m, cy->n_vec) * vec_dot(ray.dir, cy->n_vec)); 
+	return (b);
+}
+
+double	calc_a_cy(t_ray ray, t_cy *cy)
+{
+	double	a;
+
+	a = vec_dot(ray.dir, ray.dir) - pow(vec_dot(ray.dir, cy->n_vec), 2);
+	return (a);
+}
+
+double	is_hit_cylinder(t_ray ray, t_cy *cylinder)
+{
+	double	delta;
+	double	t1;
+	double	t2;
+	double	a;
+	double	b;
+
+	a = calc_a_cy(ray, cylinder);
+	b = calc_b_cy(ray, cylinder);
+	delta = calc_delta(a, b, ray, cylinder);
+	if (delta < 0)
+		return (-1);
+	t1 = (-b - sqrt(delta)) / (2 * a);
+	t2 = (-b + sqrt(delta)) / (2 * a);
+	if (t1 > 0.001)
+		return (t1);
+	if (t2 > 0.001)
+		return (t2);
+	return (-1);
+}
+
 t_hit	hit_cylinder(t_ray ray, t_cy *cylinder)
 {
 	t_hit	hit;
 
-	(void)cylinder;
-	(void)ray;
-	hit.dst = -1;
+	hit.dst = is_hit_cylinder(ray, cylinder);
+	if (hit.dst <= 0.001)
+	{
+		hit.is_hit = 0;
+		return (hit);
+	}
+	hit.point = vec_add(ray.origin, vec_mult(ray.dir, hit.dst));
 	hit.col_obj = init_color(0, 0, 0);
 	return (hit);
 }
